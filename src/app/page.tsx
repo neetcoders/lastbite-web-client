b85f68182c9ee58dcae0b8b548986cfb90ec8f88
clarkkkkk <juanfarrel0404@gmail.com>
2024-03-25 22:33:07 +0700

    Init Next project

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex w-full min-h-screen justify-center items-center">
+      <h1 className="font-semibold ">SEMANGAT LASTBITE !!!</h1>
+    </div>
+  );
+}
