a29170609c9fe3745598047b6e356e3c63addf72
keanutaufan <keanu.taufan21@gmail.com>
2024-04-24 10:55:22 +0700

    Integrate register API

--- src/app/services/userService.ts
+++ src/app/services/userService.ts
@@ -13,6 +13,14 @@ export type ILogin = {
   authorization: string;
 }
 
+export type IRegisterRequest = {
+  email: string;
+  display_name: string;
+  birth_date: string;
+  password: string;
+  confirm_password: string;
+}
+
 export type IUser = {
   email: string;
   display_name: string;
@@ -28,7 +36,7 @@ export async function login(request: ILoginRequest) {
       email: request.email,
       password: request.password,
     });
-    
+
     const token = response.data.data.authorization;
     await setAuthToken(token);
 
@@ -36,6 +44,24 @@ export async function login(request: ILoginRequest) {
   }
   catch (err) {
     console.error(err);
+    return 0;
+  }
+}
+
+export async function register(request: IRegisterRequest) {
+  try {
+    await apiClient.post("/users/register", {
+      email: request.email,
+      display_name: request.display_name,
+      birth_date: request.birth_date,
+      password: request.password,
+      confirm_password: request.confirm_password,
+    });
+
+    return 1;
+  }
+  catch (err) {
+    console.log(err);
     return 0;
   }
 }
