3f3da572c0d0e32a315bd6f055dcaacf915f3828
keanutaufan <keanu.taufan21@gmail.com>
2024-04-24 14:26:55 +0700

    Configure next/image hosts

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "lastbite.sgp1.cdn.digitaloceanspaces.com",
+        port: "",
+      },
+    ]
+  }
+};
 
 export default nextConfig;
