6662f4096bdd8d2be3f827b80f112a301e24b42c
keanutaufan <keanu.taufan21@gmail.com>
2024-04-24 10:18:32 +0700

    Add universal type declaration related to user

--- src/app/services/userService.ts
+++ src/app/services/userService.ts
@@ -1,11 +1,25 @@
 "use server";
 
 import { getAuthToken, setAuthToken, unsetAuthToken } from "./authTokenService";
-import apiClient from "./apiClient";
+import apiClient, { ResponseSchema } from "./apiClient";
+
+export type ILogin = {
+  user: IUser;
+  authorization: string;
+}
+
+export type IUser = {
+  email: string;
+  display_name: string;
+  birth_date: string;
+  active_address: null;
+  created_at: string;
+  updated_at: string;
+}
 
 export async function login(email: string, password: string) {
   try {
-    const response = await apiClient.post("/users/login", { email, password });
+    const response = await apiClient.post<ResponseSchema<ILogin>>("/users/login", { email, password });
     const token = response.data.data.authorization;
     await setAuthToken(token);
 
@@ -21,7 +35,7 @@ export async function getCurrentUser() {
   try {
     const token = await getAuthToken();
 
-    const response = await apiClient.get("/users/me", {
+    const response = await apiClient.get<ResponseSchema<IUser>>("/users/me", {
       headers: { 
         Authorization: `Bearer ${token}`
       }
