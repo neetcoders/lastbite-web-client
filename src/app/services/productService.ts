237e3bf02d13ef312a3ad891038bfb2687b4cb79
keanutaufan <keanu.taufan21@gmail.com>
2024-04-24 14:13:07 +0700

    Integrate get product details API

--- src/app/services/productService.ts
+++ src/app/services/productService.ts
@@ -11,9 +11,26 @@ export type IProduct = {
   expiration_date: string;
   stock: number;
   store: IStore;
-  category: null;
+  category: ICategory;
+  image_url?: string;
   created_at: string;
   updated_at: string;
+}
+
+export type ICategory = {
+  slug: string;
+  display_name: string;
+}
+
+export async function getProductDetails(id: string) {
+  try {
+    const response = await apiClient.get<ResponseSchema<IProduct>>(`/product/${id}`);
+    return response.data.data;
+  }
+  catch (err) {
+    console.error(err);
+    return null;
+  }
 }
 
 export async function getMyProducts() {
