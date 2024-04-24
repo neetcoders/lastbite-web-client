f525acfef2da115b05d2b8481b07532a5de0023b
keanutaufan <keanu.taufan21@gmail.com>
2024-04-24 14:29:42 +0700

    resolve merge conflict

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-<<<<<<< HEAD
     dangerouslyAllowSVG: true,
-=======
->>>>>>> a172990fbede9085c80398133805f9120a860f36
     remotePatterns: [
       {
         protocol: "https",
