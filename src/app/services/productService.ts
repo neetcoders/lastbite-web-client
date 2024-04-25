21566f5d0c20828fde5f25ca95a87966ad195394
clarkkkkk <juanfarrel0404@gmail.com>
2024-04-25 07:18:06 +0700

    Add some function related to product operation

--- src/app/services/productService.ts
+++ src/app/services/productService.ts
@@ -1,3 +1,4 @@
+import { headers } from "next/headers";
 import apiClient, { ResponseSchema } from "./apiClient";
 import { getAuthToken } from "./authTokenService";
 import { IStore } from "./storeService";
@@ -47,6 +48,61 @@ export async function getMyProducts() {
   }
   catch (err) {
     console.error(err);
+    return null;
+  }
+}
+
+export async function editProduct(id: string, formData: any) {
+  const updatedProductData = {
+    display_name: formData.name,
+    description: formData.description,
+    price_before: formData.price_before,
+    price_after: formData.price_after,
+    expiration_date: formData.exp_date,
+    // stock: formData.stock,
+    category_slug: formData.category,
+  };
+  
+  try {
+    const token = await getAuthToken();
+    const response = await apiClient.put<ResponseSchema<IProduct>>(`/product/${id}`, updatedProductData, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    return response.data.data;
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
+}
+
+export async function deleteProductById ( id : string ) {
+  try {
+    const token = await getAuthToken();
+    const response = await apiClient.delete<ResponseSchema<IProduct>>(`/product/${id}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    return response.data.data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
+export async function addNewProduct ( newProductData : any ) {
+  try {
+    const token = await getAuthToken();
+    const response = await apiClient.post<ResponseSchema<IProduct>>('/product/new', newProductData, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+    return response.data.data
+  } catch (error) {
+    console.error(error);
     return null;
   }
 }
