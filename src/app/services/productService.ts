04b384a67b3be50c13a4cd753a974c4e9c5e7f73
keanutaufan <keanu.taufan21@gmail.com>
2024-04-27 20:40:22 +0700

    fix: product API returns nothing when search query is empty

--- src/app/services/productService.ts
+++ src/app/services/productService.ts
@@ -110,7 +110,8 @@ export async function addNewProduct ( newProductData : any ) {
 
 export async function getPublicProduct ( searchedProduct? : string ){
   try {
-    const response = await apiClient.get<ResponseSchema<IProduct[]>>(`/product/public?limit=20&offset=0&distance=2000000&search=${searchedProduct || 'indomie'}`)
+    const link = searchedProduct ? `/product/public?limit=20&offset=0&distance=2000000&search=${searchedProduct}` : `/product/public?limit=20&offset=0&distance=2000000`;
+    const response = await apiClient.get<ResponseSchema<IProduct[]>>(link);
     if(response.data.data){
       return response.data.data;
     } else {
@@ -124,8 +125,9 @@ export async function getPublicProduct ( searchedProduct? : string ){
 
 export async function getUserNearestProduct (searchedProduct? : string){
   try {
-    const token = await getAuthToken()
-    const response = await apiClient.get<ResponseSchema<IProduct[]>>(`/product?limit=20&offset=0&distance=10000&search=${searchedProduct || 'indomie'}`, {
+    const token = await getAuthToken();
+    const link = searchedProduct ? `/product?limit=20&offset=0&distance=10000&search=${searchedProduct}` : `/product?limit=20&offset=0&distance=10000`;
+    const response = await apiClient.get<ResponseSchema<IProduct[]>>(link, {
       headers: {
         Authorization: `Bearer ${token}`
       }
