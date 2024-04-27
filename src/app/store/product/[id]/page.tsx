fee730180294b365311c4edba24e2e1cae27de9e
keanutaufan <keanu.taufan21@gmail.com>
2024-04-27 19:33:29 +0700

    fix: id params not passed after adding metadata

--- src/app/store/product/[id]/page.tsx
+++ src/app/store/product/[id]/page.tsx
@@ -3,6 +3,6 @@ import DetailPage from "./page.client";
 export const metadata = { title: "Product Detail" };
 
 
-export default function Page() {
-  return <DetailPage />;
+export default function Page({ params }: any) {
+  return <DetailPage params={params} />;
 }
